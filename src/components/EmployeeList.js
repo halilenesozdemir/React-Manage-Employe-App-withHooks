19473508e20d9e6ff21c422c611db33eff608ef4
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-05-12 14:45:11 +0300

    Project basic JSX correction in components.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,22 @@
 import Employee from './Employee';
 
 const EmployeeList = () => {
-  return <Employee />;
+  return (
+    <table className="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Employee />
+      </tbody>
+    </table>
+  );
 };
 
 export default EmployeeList;
